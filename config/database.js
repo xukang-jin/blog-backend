0cbe1541098266bdbe5b816f27c7c9b7b842bde6
xukang.jin <jin_xukang@163.com>
2021-04-25 21:12:42 +0800

    database

--- config/database.js
+++ config/database.js
@@ -4,12 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),// strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'blog'),
+        password: env('DATABASE_PASSWORD', 'oPqc0Am9lfhWKNuT'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
-  },
-});
+  });
